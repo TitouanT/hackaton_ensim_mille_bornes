9ec299d9e2f2d196b00e02646e3ec874e253ce09
TitouanT <titouan.teyssier@gmail.com>
2018-12-08 11:12:54 +0100

    tab hard

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 
- var http = require('http').Server(app);
+var http = require('http').Server(app);
 
- app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
- });
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
 
- http.listen(3000, function(){
-   console.log('mille bornes : listening on port 3000');
- });
+http.listen(3000, function(){
+	console.log('mille bornes : listening on port 3000');
+});
