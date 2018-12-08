3d0d70ed980f9fa3dc33f407e32d702e84b10106
TitouanT <titouan.teyssier@gmail.com>
2018-12-08 15:15:02 +0100

    template

--- deck.js
+++ deck.js
@@ -3,3 +3,21 @@ function Deck() {
 	for 
 }
 
+const template = [
+	{
+		type: "Attaques",
+		cards: [
+			{qtt: 3, name: "Accident},
+
+		]
+]
+
+
+for (ens of template) {
+	let type = ens.type;
+	for (groupe of ens.cards) {
+		for (i = 0; i < grocups.qtt; i++) {
+			cards.push(new Card(type, groupe.nam))
+		}
+	}
+}
