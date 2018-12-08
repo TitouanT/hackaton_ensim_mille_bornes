5caf68f821628c9c06fc92f46ecb7228fbe7f272
Camille <camille.bamboute@hotmail.fr>
2018-12-08 11:28:36 +0100

    commit

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+	socket.on('chat message', function(msg){
+    console.log('message: ' + msg);});
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
