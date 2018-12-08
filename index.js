736bdda2b8b829ec91bd39164ecd70a0834d91f1
Camille <camille.bamboute@hotmail.fr>
2018-12-08 11:00:55 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var pp = require('express')();
+
+ var http = require('http').Server(app);
+
+ app.get('/', function(req, res){
+   res.send('<h1> welcome to milleBOrnes</h1>');
+ });
+
+ http.listen(3000, function(){
+   console.log('mille bornes : listening on port 3000');
+ });
