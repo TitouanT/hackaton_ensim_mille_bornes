4c30458e18d377430f6dbdb724c25a7f3270bfa4
TitouanT <titouan.teyssier@gmail.com>
2018-12-08 11:20:41 +0100

    add io in code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -8,4 +8,8 @@ app.get('/', function(req, res){
 
 http.listen(3000, function(){
 	console.log('mille bornes : listening on port 3000');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
 });
